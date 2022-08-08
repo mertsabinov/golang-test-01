04d372bc7b2e99d01a7bbe4fa92cc88325fd49b2
mertovich <88425310+mertovich@users.noreply.github.com>
2022-08-08 12:13:12 +0200

    add(arry_01): SumAll

--- Array_01/array_01.go
+++ Array_01/array_01.go
@@ -7,3 +7,14 @@ func Sum(numbers []int) int {
 	}
 	return result
 }
+
+func SumAll(numberList ...[]int) []int {
+	// number list içerisinde dönen bir fonskyon yaz
+	// içerisine girilen slice ların toplamını döndür
+	// yeni bir listeye ekle
+	var sums []int
+	for _, n := range numberList {
+		sums = append(sums, Sum(n))
+	}
+	return sums
+}
--- Array_01/array_01_test.go
+++ Array_01/array_01_test.go
@@ -1,6 +1,9 @@
 package array_01
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -12,12 +15,40 @@ func TestSum(t *testing.T) {
 	if sum != want {
 		t.Errorf("sum = %d  want = %d", sum, want)
 	}
-
 }
 
 func BenchmarkSum(b *testing.B) {
 	numbers := []int{2, 2, 2}
 	for i := 0; i < b.N; i++ {
 		Sum(numbers)
+	}
+}
+
+func TestSumsAll(t *testing.T) {
+
+	sliceCheck := func(t *testing.T, sum []int, want []int) {
+		if !reflect.DeepEqual(sum, want) {
+			t.Errorf("sum = %v want = %v ", sum, want)
+		}
+	}
+
+	t.Run("slice any size", func(t *testing.T) {
+		sum := SumAll([]int{1, 1}, []int{2, 2})
+		want := []int{2, 4}
+
+		sliceCheck(t, sum, want)
+	})
+
+	t.Run("slice size zero", func(t *testing.T) {
+		sum := SumAll([]int{}, []int{1, 2, 3, 4, 5})
+		want := []int{0, 15}
+
+		sliceCheck(t, sum, want)
+	})
+}
+
+func BenchmarkSumAll(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAll([]int{2, 2}, []int{1, 2, 3, 4, 5, 6})
 	}
 }
